ebfc6669f20647b66749301c55b75a8d6dde40fb
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-05 14:25:37 -0800

    finishing first section on functions

--- Fundamentals Pt2/lessons.js
+++ Fundamentals Pt2/lessons.js
@@ -1,10 +1,30 @@
 //---------------------------------------- ACTIVATING STRICT MODE --------------------------
-'use strict'; 
+"use strict";
 
-let hasDriversLicense = false ;
-const passTest = true ;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true 
+// if (passTest) hasDriversLicense = true;
 
-if (hasDriversLicense) console.log ('they can drive ')
+// if (hasDriversLicense) console.log("they can drive ");
 
+//---------------------------------------- FUNCTIONS --------------------------
+function logger() {
+  console.log("my name is dani");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  //   console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+fruitProcessor(5, 0);
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
