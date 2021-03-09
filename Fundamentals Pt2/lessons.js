2a029b100471477d8581177fab6762de8462da7d
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-09 10:34:18 -0800

    finishing function-ception section

--- Fundamentals Pt2/lessons.js
+++ Fundamentals Pt2/lessons.js
@@ -61,3 +61,19 @@
 
 // console.log(yearsUntilRetirement(1995, "Shelly"));
 // console.log(yearsUntilRetirement(1998, "Smelly"));
+
+//----------------------------------------  FUNCTIONS  CALLING OTHER FUNCTIONS--------------------------
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
