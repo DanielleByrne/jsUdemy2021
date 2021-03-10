069dc326469db585d2a1cb7002144c47d676d1f3
Danielle Byrne <DanielleByrne421@gmail.com>
2021-03-09 18:02:45 -0800

    finishing coding challenge

--- Fundamentals Pt2/coding-challenges.js
+++ Fundamentals Pt2/coding-challenges.js
@@ -1,10 +1,10 @@
-// CODING CHALLENGE ONE 
+// CODING CHALLENGE ONE
 
-// Back to the two gymnastics teams, the Dolphins and the Koalas! 
+// Back to the two gymnastics teams, the Dolphins and the Koalas!
 //There is a new gymnastics discipline, which works differently.
-// Each team competes 3 times, and then the average of the 3 scores is calculated 
+// Each team competes 3 times, and then the average of the 3 scores is calculated
 //(so one average score per team).
-// A team only wins if it has at least double the average score of the other team. 
+// A team only wins if it has at least double the average score of the other team.
 //Otherwise, no team wins!
 
 // Your tasks:
@@ -15,9 +15,8 @@
 // 4. Use the 'checkWinner' function to determine the winner for both Data1 and Data 2
 // 5. Ignore draws this time
 
-
 //Test data:
-// Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49 
+// Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
 // Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
 
 // Hints:
@@ -25,5 +24,26 @@
 // To check if number A is at least double number B, check for A >= 2 * B.
 // Apply this to the team's average scores 😉
 
+// const calcAverage = (scoreOne, scoreTwo, scoreThree) =>
+//   (scoreOne + scoreTwo + scoreThree) / 3;
+// // console.log(calcAverage(44, 23, 71))
 
- 
+// //Test data 1
+// // const scoreDolphins = calcAverage(44, 23, 71)
+// // const scoreKoalas = calcAverage(65, 54, 49)
+
+// //Test data 2
+// const scoreDolphins = calcAverage(85, 54, 41);
+// const scoreKoalas = calcAverage(23, 34, 27);
+
+// console.log(scoreDolphins, scoreKoalas);
+
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+//   } else {
+//     console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+//   }
+// };
+
+// checkWinner(scoreDolphins, scoreKoalas);
